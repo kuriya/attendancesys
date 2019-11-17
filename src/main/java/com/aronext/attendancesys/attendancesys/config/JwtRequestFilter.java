e2e65f0d1ce40c9bfb7f8e2a786bce95db929842
dilshann <dilshan.niroda@pearson.com>
2019-11-17 20:40:20 +0530

    refactoring

--- src/main/java/com/aronext/attendancesys/attendancesys/config/JwtRequestFilter.java
+++ src/main/java/com/aronext/attendancesys/attendancesys/config/JwtRequestFilter.java
@@ -32,15 +32,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private JwtTokenUtil jwtTokenUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         final String jwtToken = request.getHeader("x-authorization");
         String username = null;
-// JWT Token is in the form "Bearer token". Remove Bearer word and get
-// only the Token
-//        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
         if (jwtToken != null) {
-//            jwtToken = requestTokenHeader.substring(7);
             try {
                 username = jwtTokenUtil.getUsernameFromToken(jwtToken);
             } catch (IllegalArgumentException e) {
@@ -51,19 +46,13 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         } else {
             logger.warn("JWT Token does not begin with Bearer String");
         }
-// Once we get the token validate it.
+
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.jwtUserDetailsService.loadUserByUsername(username);
-// if token is valid configure Spring Security to manually set
-// authentication
+
             if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken
-                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-// After setting the Authentication in the context, we specify
-// that the current user is authenticated. So it passes the
-// Spring Security Configurations successfully.
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
--- src/main/java/com/aronext/attendancesys/attendancesys/config/JwtTokenUtil.java
+++ src/main/java/com/aronext/attendancesys/attendancesys/config/JwtTokenUtil.java
@@ -12,7 +12,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
@@ -23,7 +22,6 @@ import java.util.function.Function;
 @Component
 public class JwtTokenUtil implements Serializable {
     private static final long serialVersionUID = -2550185165626007488L;
-//    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
     @Value("${jwt.secret}")
     private String secret;
@@ -31,12 +29,20 @@ public class JwtTokenUtil implements Serializable {
     @Value("${jwt.token.expiration.in.minute}")
     private long expiration;
 
-
-    //retrieve username from jwt token
+    /**
+     * retrieve username from jwt token
+     * @param token
+     * @return
+     */
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
-    //retrieve expiration date from jwt token
+
+    /**
+     * retrieve expiration date from jwt token
+     * @param token
+     * @return
+     */
     public Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
@@ -44,32 +50,60 @@ public class JwtTokenUtil implements Serializable {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
-    //for retrieveing any information from token we will need the secret key
+
+    /**
+     * for retrieveing any information from token we will need the secret key
+     * @param token
+     * @return
+     */
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
-    //check if the token has expired
+
+    /**
+     * check if the token has expired
+     * @param token
+     * @return
+     */
     private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
-    //generate token for user
+
+    /**
+     * generate token for user
+     * @param userDetails
+     * @return
+     */
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return doGenerateToken(claims, userDetails.getUsername());
     }
-    //while creating the token -
-//1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
-//2. Sign the JWT using the HS512 algorithm and secret key.
-//3. According to JWS Compact Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
-//   compaction of the JWT to a URL-safe string
+
+    /**
+     *  while creating the token -
+     * 1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
+     * 2. Sign the JWT using the HS512 algorithm and secret key.
+     * 3. According to JWS Compact Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
+     * 4. compaction of the JWT to a URL-safe string
+     * @param claims
+     * @param subject
+     * @return
+     */
+
     private String doGenerateToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
 //                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
                 .setExpiration(new Date(System.currentTimeMillis() + expiration * 60 * 1000))
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
     }
-    //validate token
+
+    /**
+     * validate token
+     * @param token
+     * @param userDetails
+     * @return
+     */
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
