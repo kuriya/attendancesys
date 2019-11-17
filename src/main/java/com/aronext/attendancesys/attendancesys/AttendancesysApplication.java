fc956e8eeaab45102275932b43c3d2fab44e8b60
dilshann <dilshan.niroda@pearson.com>
2019-11-17 12:31:47 +0530

    initial commit

---
+++ src/main/java/com/aronext/attendancesys/attendancesys/AttendancesysApplication.java
@@ -0,0 +1,13 @@
+package com.aronext.attendancesys.attendancesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendancesysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aronext/attendancesys/attendancesys/AttendancesysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aronext.attendancesys.attendancesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancesysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
