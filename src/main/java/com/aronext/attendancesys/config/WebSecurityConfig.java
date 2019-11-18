cdd49387c6c179f0ec3a0cd6345a845472190fdc
dilshann <dilshan.niroda@pearson.com>
2019-11-18 23:28:54 +0530

    allow static resources

--- src/main/java/com/aronext/attendancesys/config/WebSecurityConfig.java
+++ src/main/java/com/aronext/attendancesys/config/WebSecurityConfig.java
@@ -68,7 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().authorizeRequests()
-             .antMatchers("/authenticate","/login").permitAll()
+             .antMatchers("/authenticate","/login","/favicon.ico","/bootstrap/**", "/dist/**","/plugins/**").permitAll()
             .anyRequest().authenticated().and()
             .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
