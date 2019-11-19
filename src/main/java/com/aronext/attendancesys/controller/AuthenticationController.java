4cc86ede15dcca653df58799bb1361554f069f27
dilshann <dilshan.niroda@pearson.com>
2019-11-19 18:55:19 +0530

    integrate with the login and dashboard

--- src/main/java/com/aronext/attendancesys/controller/AuthenticationController.java
+++ src/main/java/com/aronext/attendancesys/controller/AuthenticationController.java
@@ -20,6 +20,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
@@ -46,6 +47,29 @@ public class AuthenticationController {
         final String token = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new JwtResponse(token));
     }
+
+    @RequestMapping(value = "/loginAuthenticate", method = RequestMethod.POST)
+    public ModelAndView authentication(@Valid @ModelAttribute JwtRequest authenticationRequest)  {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        }catch (Exception e){
+            modelAndView.setViewName("login");
+            return modelAndView;
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        if(token !=null){
+            modelAndView.setViewName("dashboard/dashboard");
+        }else{
+            modelAndView.setViewName("login");
+        }
+        return modelAndView;
+    }
+
+
+
+
     private void authenticate(String username, String password) throws Exception {
         try {
 
